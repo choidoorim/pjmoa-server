bf87c87b1d6a2108dc1c51725c744d6274e17baf
choidoorim <doorim321@naver.com>
2021-11-19 00:54:09 +0900

    Update: entity 경로 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from "joi";
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + 'src/entity/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV == 'prod' ? '.env.prod' : '.env.dev',
@@ -26,6 +16,16 @@ import * as Joi from "joi";
         DATABASE_PASSWORD: Joi.string().required(),
         DATABASE_NAME: Joi.string().required(),
       }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [`dist/entity/**/*.{ts,js}`],
+      synchronize: true,
     }),
     UserModule,
   ],
