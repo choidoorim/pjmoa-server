7b5b98fab862835c0d157ec260dda905044429c7
choidoorim <doorim321@naver.com>
2021-11-24 10:58:46 +0900

    Update: Repository 로 DB 관련 연산들 이동

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -12,16 +12,38 @@ import { Injectable } from '@nestjs/common';
 export class UserRepository extends Repository<User> {
   public async doUserRegistration(
     @TransactionManager() transactionManager: EntityManager,
-    createUserInfo,
+    registerUserInfo,
   ) {
+    const createUserInfo: User[] = this.create(registerUserInfo);
+
     return await transactionManager.save(createUserInfo);
   }
 
   public async updateUserProfile(
-      @TransactionManager() transactionManager: EntityManager,
-      updateUserInfo,
+    @TransactionManager() transactionManager: EntityManager,
+    userInfo,
+    userIdx,
   ) {
+    const updateUserInfo: User = await this.findUser(userIdx);
+    updateUserInfo.email = userInfo.email;
+    updateUserInfo.firstName = userInfo.firstName;
+    updateUserInfo.lastName = userInfo.lastName;
+    updateUserInfo.age = userInfo.age;
+    updateUserInfo.phoneNumber = userInfo.phoneNumber;
+
     return await transactionManager.save(updateUserInfo);
   }
 
+  public async findUser(userIdx: number) {
+    return await this.findOne(userIdx);
+  }
+
+  public async findUserPassword(email: string) {
+    const [userPasswordResult] = await this.find({
+      select: ['password'],
+      where: { email: email, status: true },
+    });
+
+    return userPasswordResult;
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,35 +1,28 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { getConnection, Connection, Repository } from 'typeorm';
+import { getConnection } from 'typeorm';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import { CreateUserDTO } from 'src/dto/user/create-user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(
-    private connection: Connection,
-    private usersRepository: UserRepository,
-  ) {}
+  constructor(private usersRepository: UserRepository) {}
 
-  async doUserRegistration(registerUserInfo: CreateUserDTO): Promise<User> {
-    const queryRunner = await this.connection.createQueryRunner();
+  async doUserRegistration(registerUserInfo: CreateUserDTO): Promise<User[]> {
+    const queryRunner = await getConnection().createQueryRunner();
     await queryRunner.startTransaction();
     try {
       // create: Active Record 에서 entity 에 BaseEntity 를 상속해서 사용하지 않도록 한다.
-      const createUserInfo: User = await this.usersRepository.create(
+      const result = await this.usersRepository.doUserRegistration(
+        queryRunner.manager,
         registerUserInfo,
       );
 
-      const result: User = await this.usersRepository.doUserRegistration(
-        queryRunner.manager,
-        createUserInfo,
-      );
       await queryRunner.commitTransaction();
       return result;
     } catch (error) {
       await queryRunner.rollbackTransaction();
-      throw new NotFoundException(`Failed SignUp ${error}`);
+      throw new NotFoundException(`Failed SignUp - ${error}`);
     } finally {
       await queryRunner.release();
     }
@@ -51,42 +44,27 @@ export class UserService {
     return userProfileResult;
   }
 
-  // update 는 find 와 save 조합을 이용.
-  async findUser(userIdx: number): Promise<User> {
-    return await this.usersRepository.findOne(userIdx);
-  }
-
-  async updateUserProfile(userInfo, userIdx) {
+  async updateUserProfile(userInfo, userIdx): Promise<User> {
     const queryRunner = await getConnection().createQueryRunner();
     await queryRunner.startTransaction();
     try {
-      const updateUserInfo: User = await this.findUser(userIdx);
-      updateUserInfo.email = userInfo.email;
-      updateUserInfo.firstName = userInfo.firstName;
-      updateUserInfo.lastName = userInfo.lastName;
-      updateUserInfo.age = userInfo.age;
-      updateUserInfo.phoneNumber = userInfo.phoneNumber;
-
       const updateUserResult = await this.usersRepository.updateUserProfile(
         queryRunner.manager,
-        updateUserInfo,
+        userInfo,
+        userIdx,
       );
 
       await queryRunner.commitTransaction();
       return updateUserResult;
     } catch (error) {
       await queryRunner.rollbackTransaction();
-      throw new NotFoundException(`Failed SignUp ${error}`);
+      throw new NotFoundException(`Failed Update UserProfile - ${error}`);
     } finally {
       await queryRunner.release();
     }
   }
 
   async findUserPassword(email: string): Promise<User> {
-    const [userPasswordResult] = await this.usersRepository.find({
-      select: ['password'],
-      where: { email: email, status: true },
-    });
-    return userPasswordResult;
+    return await this.usersRepository.findUserPassword(email);
   }
 }
